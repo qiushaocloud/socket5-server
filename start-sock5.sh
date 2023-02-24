ad589e716d98f574cdedc11f56c7c1e8599bf2e6
qiushaocloud <qiushaocloud@126.com>
2023-02-25 02:55:03 +0800

    update dockerfile

--- start-sock5.sh
+++ start-sock5.sh
@@ -1,9 +1,13 @@
-if [ "$SOCKS_USER" ] && [ "$SOCKS_PASS" ]; then
-    cp -a /etc/danted.conf.user /etc/danted.conf
-else
-    cp -a /etc/danted.conf.nouser /etc/danted.conf
+if [ ! -f "/etc/danted.conf" ];then
+    echo "file /etc/danted.conf is not exist"
+    if [ "$SOCKS_USER" ] && [ "$SOCKS_PASS" ]; then
+        cp -a /etc/danted.conf.user /etc/danted.conf
+    else
+        cp -a /etc/danted.conf.nouser /etc/danted.conf
+    fi
+
+    sed -i "s/<SOCKS_PORT>/$SOCKS_PORT/" /etc/danted.conf
 fi
 
-sed -i "s/<SOCKS_PORT>/$SOCKS_PORT/" /etc/danted.conf
 
 danted -f /etc/danted.conf -D
